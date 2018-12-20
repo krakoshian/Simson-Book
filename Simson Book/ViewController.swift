c1f273deb2567db1be5f18a6d67069f7a0c23ebe
Kadir Uludağ <kadiruludag@kadir-macbook-pro.local>
2018-12-20 20:27:46 +0300

    Initial Commit

---
+++ Simson Book/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simson Book
+//
+//  Created by Kadir Uludağ on 20.12.2018.
+//  Copyright © 2018 Kadir Uludağ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
